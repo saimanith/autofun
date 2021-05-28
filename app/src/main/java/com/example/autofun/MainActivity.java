8b6d539cf6c5dee1eab79231fd8ee4ece7973fc9
saimanith <saimanith.bunty921@gmail.com>
2021-05-28 07:31:01 +0530

    autofun

---
+++ app/src/main/java/com/example/autofun/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.autofun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        String[] friends = {"Nick","Noel","Rita","Nire","Ramu"};
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,friends);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        autoCompleteTextView.setThreshold(1);
+    }
+}
